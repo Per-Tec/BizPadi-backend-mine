eddca28b0e51afd5f3c568f449fdac612952463c
Akinbayo Clinton <clintexakinbs@gmail.com>
2025-04-08 02:26:07 +0100

    chore: setup passport google auth configs

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const passport = require("./configs/passport");
 
 require("dotenv").config();
 
-const { API_VERSION } = process.env;
+const { API_VERSION, SESSION_SECRET} = process.env;
 
 //Routers
 const authRouter = require("./routes/auth.route");
@@ -19,19 +21,32 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
     res.send("Welcome to BizPadi API");
-}); 
+});
 
 app.use(`/api/v${API_VERSION}/auth/`, authRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // app.use('**', (req, res) => {
 //     res.status(404).json({
-//         success: false,
-//          message: "Route Not Found"
-//         });
+//         status: "error",
+//         message: "Route not found",
+//     });
 // })
 
 module.exports = app
---
+++ src/configs/passport.js
@@ -0,0 +1,86 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const User = require("../models/user");
+const { v4: uuidv4 } = require("uuid");
+const logger = require("../utils/logger");
+
+require("dotenv").config();
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } =
+  process.env;
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(async (user_id, done) => {
+  try {
+    const user = await User.findOne({
+      where: { user_id },
+    });
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACK_URL,
+      profileFields: ["id", "displayName", "photos", "emails"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        logger.info(`START: Attempting to log in a user with Google`);
+        const existingUser = await User.findOne({
+          where: {
+            email: profile.emails[0].value,
+          },
+        });
+
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        const names = profile.displayName.split(" ");
+        const first_name = names[0];
+        const last_name = names.length > 1 ? names[names.length - 1] : "";
+
+        const timestamp = new Date().toISOString();
+
+        //Create new User with google data
+        const user_id = uuidv4();
+        const newUser = await User.create({
+          user_id,
+          first_name,
+          last_name,
+          business_name: "", // Will need to be set later
+          industry: "", // Will need to be set later
+          email: profile.emails[0].value,
+          email_verified: true, // Google has verified this email
+          phone_number: "", // Will need to be set later
+          password_hash: "", // No password for social login
+          google_id: profile.id,
+          profile_photo:
+            profile.photos && profile.photos[0]
+              ? profile.photos[0].value
+              : null,
+          created_at: timestamp,
+          updated_at: timestamp,
+          account_type: "google", // Set account type to google
+        });
+        logger.info(`END: Successfully logged in user with Google`);
+        done(null, newUser);
+      } catch (error) {
+        logger.error(
+          `END: Error logging in user with Google: ${error.message}\nStack trace: ${error.stack}`
+        );
+        done(error, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
